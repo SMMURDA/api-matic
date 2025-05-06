a85cd5010d5d931c44afa5c4d7901bf038deeb6f
root <mustofabovalone@gmail.com>
2025-05-07 00:51:35 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const whoisRoute = require('./public/routes/whois');
+const embedRoute = require('./public/routes/embed');
+const shortenRoute = require('./public/routes/shorten');
+const randomUserRoute = require('./public/routes/randomUser'); // Ganti qrcodeRoute dengan randomUserRoute
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Static files (untuk dokumentasi API)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
+app.use('/api/whois', whoisRoute);
+app.use('/api/embed', embedRoute);
+app.use('/api/shorten', shortenRoute);
+app.use('/api/randomuser', randomUserRoute); // Ganti dengan route randomUser
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Selamat datang di API server');
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
